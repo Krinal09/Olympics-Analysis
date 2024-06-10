efae315a9eac6504fcff5cf024c4480871dc0bd2
Krinal09 <krinalgami@gmail.com>
2024-06-10 21:56:09 +0530

    First commit

---
+++ preprocessor.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+def preprocess(df, region_df):
+    
+    # global df, region_df
+    
+    df = df[df['Season'] == 'Summer']
+    
+    df = df.merge(region_df, on = 'NOC', how= 'left')
+    
+    df.drop_duplicates(inplace=True)
+    
+    df = pd.concat([df, pd.get_dummies(df['Medal'])], axis = 1)
+    
+    return df
